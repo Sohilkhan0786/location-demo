c85d9de7d5d0bb44e10ec43297c7ff4ee7f6ed03
Sohilkhan0786 <kkhan630011@gmail.com>
2026-09-22 16:10:38 +0530

    Add files via upload

---
+++ server.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const path = require("path");
+
+const app = express();
+const PORT = 5000;
+
+app.use(express.json());
+
+app.use(express.static(path.join(__dirname, "public")));
+
+let latestLocation = null;
+
+app.post("/api/location", (req, res) => {
+    const { latitude, longitude, accuracy } = req.body;
+
+    latestLocation = {
+        latitude,
+        longitude,
+        accuracy,
+        time: new Date().toLocaleString()
+    };
+
+    console.log("Location received:");
+    console.log(latestLocation);
+
+    res.json({
+        success: true
+    });
+});
+
+app.get("/api/location", (req, res) => {
+    res.json(latestLocation);
+});
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`Server running on http://localhost:${PORT}`);
+});

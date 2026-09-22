be0f486892e120226ac6d8498908cedb6ac9d5c8
Sohilkhan0786 <kkhan630011@gmail.com>
2026-09-22 16:52:24 +0530

    Update server.js

--- server.js
+++ server.js
@@ -6,7 +6,7 @@ const PORT = process.env.PORT || 5000;
 
 app.use(express.json());
 
-app.use(express.static(path.join(__dirname, "public")));
+app.use(express.static(__dirname));
 
 let latestLocation = null;
 
